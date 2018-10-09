72712f83f8679656dbcf5ae5e61b7d4004420372
Vincent Van Steenbergen <v.vansteenbergen@gmail.com>
2018-10-09 09:53:46 +0200

    Update stuff

--- v2/assets/js/animated-headlines.js
+++ v2/assets/js/animated-headlines.js
@@ -1,15 +1,15 @@
 jQuery(document).ready(function($){
 	//set animation timing
-	var animationDelay = 2500,
+	var animationDelay = 2000,
 		//loading bar effect
 		barAnimationDelay = 3800,
 		barWaiting = barAnimationDelay - 3000, //3000 is the duration of the transition on the loading bar - set in the scss/css file
 		//letters effect
 		lettersDelay = 50,
 		//type effect
-		typeLettersDelay = 150,
-		selectionDuration = 500,
-		typeAnimationDelay = selectionDuration + 800,
+		typeLettersDelay = 100,
+		selectionDuration = 100,
+		typeAnimationDelay = selectionDuration + 500,
 		//clip effect 
 		revealDuration = 600,
 		revealAnimationDelay = 1500;
