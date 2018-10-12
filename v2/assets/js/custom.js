aaee2338ee1bcecbfaf6c699ba27f064aa1e9d16
Florian Longuet <florian@thinkdeep.ai>
2018-10-12 17:14:16 +0200

    Add our project popup
    Wip of the popup ( only on the first image )
    + Add work with us section

--- v2/assets/js/custom.js
+++ v2/assets/js/custom.js
@@ -198,7 +198,11 @@
 		$(".navbar-nav").on('click', 'a', function(){
 			$(".navbar-collapse").removeClass("in");
 		})
-		
+		// POPUP
+		$('.open-popup-link').magnificPopup({
+  type:'inline',
+  midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
+});
 		
 	});
 
